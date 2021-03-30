5234148d9c8deae376fe7ed877757239acc36bdf
Jonathan Kelly <12926868+BitJetKit@users.noreply.github.com>
2021-03-30 05:56:38 -0500

    Give an opening comment.

--- Test.java
+++ Test.java
@@ -1,5 +1,12 @@
 package DatingEquation;
 
+/**
+ * @author: Bit Jet Kit
+ * Title: Test
+ * Date: March 30, 2021
+ * Purpose: Implement the Dating Equation.
+ */
+
 public class Test {
     public static void main(String[] args) {
         DatingEquation potentialDate = new DatingEquation();
